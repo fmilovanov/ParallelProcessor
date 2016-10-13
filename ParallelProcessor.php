fb70a26c82d65eea7d5e0758ab7157be3bd2fe48
Felix <Felix@Skylake>
2016-10-13 09:41:26 -0400

    implement class, needs testing

--- ParallelProcessor.php
+++ ParallelProcessor.php
@@ -5,11 +5,41 @@
 
 abstract class ParallelProcessor
 {
-    private $__nprocesses;
+    const DEFAULT_IPC_ID    = 0xdead;
 
-    public function __construct($nprocesses)
+    private $__ipc_id;
+    private $__mess_queue;
+    private $__processes;
+
+    public function __construct($nprocesses, $ipc_id = self::DEFAULT_IPC_ID)
     {
-        $this->__nprocesses = $nprocesses;
+        if (!function_exists('pcntl_fork()'))
+            throw new \Exception('pcntl_fork() not found; please, install library');
+
+        if (!function_exists('msg_get_queue'))
+            throw new \Exception('msg_get_queue() not found; please, install library');
+
+        // create message queue
+        $this->__ipc_id = $ipc_id;
+        $this->__mess_queue = msg_get_queue($ipc_id);
+
+        for ($i = 0; $i < $nprocesses; $i++)
+        {
+            if ($pid = pcntl_fork())
+            {
+                $this->__processes[$pid] = true;
+                continue;
+            }
+
+            // in a child - wait for jobs and execute them
+            while(msg_receive($this->__mess_queue, 0, $msgtype, 16 * 1024, $data))
+            {
+                $this->executeJob($data);
+            }
+
+            // exit once message queue is destroyed - e.g. all jobs are processed
+            exit();
+        }
     }
 
     protected function lock()
@@ -19,17 +49,36 @@ abstract class ParallelProcessor
 
     protected function release()
     {
-        
+
     }
 
     public function runJob($data)
     {
-
+        msg_send($this->__mess_queue, 1, $data);
     }
 
     public function completeJobs()
     {
+        while(1)
+        {
+            // check if we have any messages in the queue
+            $stat = msg_stat_queue($this->__mess_queue);
+            if (!$stat['msg_qnum'])
+                break;
 
+            // if yes - sleep 1/10 of second
+            usleep(100000);
+        }
+
+        // remove message queue - this will effectively send children a signal to kill themselves
+        msg_remove_queue($this->__mess_queue);
+
+        while(!empty($this->__processes))
+        {
+            $pid = pcntl_wait($status);
+            if (array_key_exists($pid, $this->__processes))
+                unset($this->__processes[$pid]);
+        }
     }
 
     abstract protected function executeJob($data);
